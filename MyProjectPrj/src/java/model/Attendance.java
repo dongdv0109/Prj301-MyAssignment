018ef075272304293269bd13a03500acd55351fe
dongdv0109 <dongnbka@gmail.com>
2022-10-28 03:18:52 +0700

    takeattend for teacher

--- MyProjectPrj/src/java/model/Attendance.java
+++ MyProjectPrj/src/java/model/Attendance.java
@@ -8,6 +8,43 @@ package model;
  *
  * @author Dong
  */
-public class Attendance {
+public class Attandance {
+    private Student student;
+    private Session session;
+    private boolean present;
+    private String description;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
+    public boolean isPresent() {
+        return present;
+    }
+
+    public void setPresent(boolean present) {
+        this.present = present;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
     
 }
+
--- MyProjectPrj/src/java/model/Lecturer.java
+++ MyProjectPrj/src/java/model/Lecturer.java
@@ -4,10 +4,49 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Dong
  */
 public class Lecturer {
+    private int id;
+    private String name;
+    private ArrayList<Group> groups = new ArrayList<>();
+    private ArrayList<Session> sessions = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(ArrayList<Group> groups) {
+        this.groups = groups;
+    }
+
+    public ArrayList<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(ArrayList<Session> sessions) {
+        this.sessions = sessions;
+    }
     
 }
+
